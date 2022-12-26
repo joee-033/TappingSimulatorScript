d6fbaf87296c0c33bc56550c0fb275c47f636c05
joee-033 <115586912+joee-033@users.noreply.github.com>
2022-12-26 21:41:35 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,6 +2,16 @@
 getgenv().autoTapEnabled = false;
 getgenv().eggShopsTable = game:GetService("Workspace").Shops:GetChildren()
 
+
+-- Commonly-Used Variables
+local playerList = game:GetService("Players")
+local user = playerList.LocalPlayer
+
+
+-- Instance Spoofer, credits to Newmat on V3RM
+loadstring(game:HttpGet("https://pastebin.com/raw/tUUGAeaH", true))()
+
+
 -- User Interface Library Initilization / Configurations
 local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/cypherdh/VanisUILIB/main/.gitignore"))()
 local ui = lib:CreateWindow(string.format("Welcome, %s. Enjoy Tapping Simulator  ", game:GetService("Players").LocalPlayer.Name), "  v1")
@@ -11,7 +21,15 @@ local farmingPage = tab:CreateFrame("Farming")
 local teleportsPage = tab:CreateFrame("Teleports")
 local settingsPage = tab:CreateFrame("Settings")
 
+
 -- Local Player Movement Page
+local walkSpeedSlider = movementPage:CreateSlider("Change Local-Player Walking Speed", 16, 500, function(val)
+    spoof(game.Players.LocalPlayer.Character.Humanoid, "WalkSpeed", val)
+end)
+
+local jumpPowerSlider = movementPage:CreateSlider("Change Local-Player Jump Power", 50, 500, function(val)
+    spoof(game.Players.LocalPlayer.Character.Humanoid, "JumpPower", val)
+end)
 
 
 -- Tapping Simulator Farming Page  ==>  ( farmingPage )
@@ -20,8 +38,6 @@ local autoTapperToggle = farmingPage:CreateToggle("Automatic Tapping", "Automatt
 	autoTapper()
 end)
 
-
--- Automatic Tapping / Farming
 function autoTapper()
     spawn(function()
         while autoTapEnabled == true do
